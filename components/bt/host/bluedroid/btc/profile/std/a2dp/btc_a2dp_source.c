55f9c1e9b56f4e1683de90f40dcbc6133e129b4c
Dmitriy Kolmakov <kolmakov125@gmail.com>
2025-08-26 19:19:47 +0300

    EHUB-78 fix(btc_a2dp_source): optimize memory usage in a2dp audio task

--- components/bt/host/bluedroid/btc/profile/std/a2dp/btc_a2dp_source.c
+++ components/bt/host/bluedroid/btc/profile/std/a2dp/btc_a2dp_source.c
@@ -77,11 +77,11 @@ static const char *TAG = "BTC_A2DP_SOURCE";
 
 /* Media task tick in milliseconds, must be set to multiple of
    (1000/TICKS_PER_SEC) */
-#define BTC_MEDIA_TIME_TICK_MS                 (30)
+#define BTC_MEDIA_TIME_TICK_MS                 (20)
 #define A2DP_DATA_READ_POLL_MS                 (BTC_MEDIA_TIME_TICK_MS / 2)
 
 #ifndef MAX_PCM_FRAME_NUM_PER_TICK
-#define MAX_PCM_FRAME_NUM_PER_TICK             21 // 14 for 20ms
+#define MAX_PCM_FRAME_NUM_PER_TICK             14 // 14 for 20ms
 #endif
 
 #define BTC_MEDIA_AA_BUF_SIZE                  (4096+16)
@@ -118,7 +118,7 @@ static const char *TAG = "BTC_A2DP_SOURCE";
 
 /* 5 frames is equivalent to 6.89*5*2.9 ~= 100 ms @ 44.1 khz, 20 ms mediatick */
 #define MAX_OUTPUT_A2DP_FRAME_QUEUE_SZ         (5)
-#define MAX_OUTPUT_A2DP_SRC_FRAME_QUEUE_SZ     (27) // 18 for 20ms tick
+#define MAX_OUTPUT_A2DP_SRC_FRAME_QUEUE_SZ     (18) // 18 for 20ms tick
 
 #define BTC_A2DP_SRC_DATA_QUEUE_IDX            (1)
 
