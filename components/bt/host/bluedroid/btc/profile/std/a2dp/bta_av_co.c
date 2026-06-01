e7d377ab4c512b17bfe1057966ce3e21c17ebb37
Dmitriy Kolmakov <kolmakov125@gmail.com>
2026-06-01 19:37:07 +0000

    EAR-247 feat: make a2dp sink connect with 44.1 only

--- components/bt/host/bluedroid/btc/profile/std/a2dp/bta_av_co.c
+++ components/bt/host/bluedroid/btc/profile/std/a2dp/bta_av_co.c
@@ -81,7 +81,7 @@ const tA2D_SBC_CIE bta_av_co_sbc_caps = {
 
 /* SBC SINK codec capabilities */
 const tA2D_SBC_CIE bta_av_co_sbc_sink_caps = {
-    (A2D_SBC_IE_SAMP_FREQ_48 | A2D_SBC_IE_SAMP_FREQ_44), /* samp_freq */
+    (A2D_SBC_IE_SAMP_FREQ_44), /* samp_freq */
     (A2D_SBC_IE_CH_MD_MONO | A2D_SBC_IE_CH_MD_STEREO | A2D_SBC_IE_CH_MD_JOINT | A2D_SBC_IE_CH_MD_DUAL), /* ch_mode */
     (A2D_SBC_IE_BLOCKS_16 | A2D_SBC_IE_BLOCKS_12 | A2D_SBC_IE_BLOCKS_8 | A2D_SBC_IE_BLOCKS_4), /* block_len */
     (A2D_SBC_IE_SUBBAND_4 | A2D_SBC_IE_SUBBAND_8), /* num_subbands */
